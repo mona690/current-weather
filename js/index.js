a38f9980aef3d7263fd25b0bdb9527867fcc9aef
mona690 <66241299+mona690@users.noreply.github.com>
2024-06-13 22:02:05 +0200

    Describe your changes here

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
-async function search(a) {
-  let t = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=9418ac7964bd403fbda211126241206&q=${a}&days=3`
+async function search(location) {
+  let response = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=9418ac7964bd403fbda211126241206&q=${location}&days=3`
   );
-  if (t.ok && 400 != t.status) {
-    let a = await t.json();
-    displayCurrent(a.location, a.current),
-      displayAnother(a.forecast.forecastday);
+  if (response.ok && response.status !== 400) {
+    let data = await response.json();
+    displayCurrent(data.location, data.current);
+    displayAnother(data.forecast.forecastday);
   }
 }
-document.getElementById("search").addEventListener("keyup", (a) => {
-  search(a.target.value);
+
+document.getElementById("search").addEventListener("keyup", (event) => {
+  search(event.target.value);
 });
+
 var days = [
   "Sunday",
   "Monday",
@@ -34,53 +36,83 @@ const monthNames = [
   "November",
   "December",
 ];
-function displayCurrent(a, t) {
-  if (null != t) {
-    var e = new Date(t.last_updated.replace(" ", "T"));
-    let n = `<div class="today forecast">\n    <div class="forecast-header"  id="today">\n    <div class="day">${
-      days[e.getDay()]
-    }</div>\n    <div class=" date">${
-      e.getDate() + monthNames[e.getMonth()]
-    }</div>\n    </div> \x3c!-- .forecast-header --\x3e\n    <div class="forecast-content" id="current">\n    <div class="location">${
-      a.name
-    }</div>\n    <div class="degree">\n        <div class="num">${
-      t.temp_c
-    }<sup>o</sup>C</div>\n      \n        <div class="forecast-icon">\n            <img src="https:${
-      t.condition.icon
-    }" alt="" width=90>\n        </div>\t\n    \n    </div>\n    <div class="custom">${
-      t.condition.text
-    }</div>\n    <span><img src="images/icon-umberella.png" alt="">20%</span>\n\t\t\t\t\t\t\t\t<span><img src="images/icon-wind.png" alt="">18km/h</span>\n\t\t\t\t\t\t\t\t<span><img src="images/icon-compass.png" alt="">East</span>\n    </div>\n</div>`;
+
+function displayCurrent(location, current) {
+  if (current != null) {
+    var e = new Date(current.last_updated.replace(" ", "T"));
+    let n = `<div class="today forecast">
+      <div class="forecast-header" id="today">
+        <div class="day">${days[e.getDay()]}</div>
+        <div class="date">${e.getDate()} ${monthNames[e.getMonth()]}</div>
+      </div>
+      <div class="forecast-content" id="current">
+        <div class="location">${location.name}</div>
+        <div class="degree">
+          <div class="num">${current.temp_c}<sup>o</sup>C</div>
+          <div class="forecast-icon">
+            <img src="https:${current.condition.icon}" alt="" width="90">
+          </div>
+        </div>
+        <div class="custom">${current.condition.text}</div>
+        <span><img src="images/icon-umberella.png" alt="">20%</span>
+        <span><img src="images/icon-wind.png" alt="">18km/h</span>
+        <span><img src="images/icon-compass.png" alt="">East</span>
+      </div>
+    </div>`;
     document.getElementById("forecast").innerHTML = n;
   }
 }
-function displayAnother(a) {
+
+function displayAnother(forecastDays) {
   let t = "";
-  for (let e = 1; e < a.length; e++)
-    t += `\t<div class="forecast">\n        <div class="forecast-header">\n            <div class="day">${
-      days[new Date(a[e].date.replace(" ", "T")).getDay()]
-    }</div>\n        </div> \x3c!-- .forecast-header --\x3e\n        <div class="forecast-content">\n            <div class="forecast-icon">\n                <img src="https:${
-      a[e].day.condition.icon
-    }" alt="" width=48>\n            </div>\n            <div class="degree">${
-      a[e].day.maxtemp_c
-    }<sup>o</sup>C</div>\n            <small>${
-      a[e].day.mintemp_c
-    }<sup>o</sup></small>\n            <div class="custom">${
-      a[e].day.condition.text
-    }</div>\n        </div>\n        </div>`;
+  for (let e = 1; e < forecastDays.length; e++) {
+    t += `<div class="forecast">
+      <div class="forecast-header">
+        <div class="day">${days[new Date(forecastDays[e].date.replace(" ", "T")).getDay()]}</div>
+      </div>
+      <div class="forecast-content">
+        <div class="forecast-icon">
+          <img src="https:${forecastDays[e].day.condition.icon}" alt="" width="48">
+        </div>
+        <div class="degree">${forecastDays[e].day.maxtemp_c}<sup>o</sup>C</div>
+        <small>${forecastDays[e].day.mintemp_c}<sup>o</sup></small>
+        <div class="custom">${forecastDays[e].day.condition.text}</div>
+      </div>
+    </div>`;
+  }
   document.getElementById("forecast").innerHTML += t;
 }
-search("cairo");
-/* Ariona Compass Starter Bootstrap */
 
+// Get user's location and fetch weather
+function getLocationAndWeather() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        search(`${lat},${lon}`);
+      },
+      (error) => {
+        alert("Unable to retrieve your location. Please enter a location manually.");
+        console.error(error);
+      }
+    );
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+// Fetch weather for user's location on page load
+window.onload = getLocationAndWeather;
+
+// Existing code for background image/color generator
 +(function () {
-  /* Data background image generator */
   var elBgImg = "[data-bg-image]";
   $(elBgImg).each(function () {
     var image = $(this).data("bg-image");
     $(this).css("background-image", "url(" + image + ")");
   });
 
-  /* Data background color generator */
   var elBgClr = "[data-bg-color]";
   $(elBgClr).each(function () {
     var color = $(this).data("bg-color");
@@ -88,7 +120,6 @@ search("cairo");
   });
 
   $(window).load(function () {
-    /* Filterable Items */
     var $container = $(".filterable-items");
     $container.isotope({
       filter: "*",
@@ -99,6 +130,7 @@ search("cairo");
         queue: false,
       },
     });
+
     $(".filterable-nav a").click(function (e) {
       e.preventDefault();
       $(".filterable-nav .current").removeClass("current");
